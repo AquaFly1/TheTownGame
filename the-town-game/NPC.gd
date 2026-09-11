91e40b68213d8f386db152b8acd25922a279db18
AquaFly1 <louis.emery@hotmail.com>
2026-09-11 12:27:15 -0400

    COntinue NPC, start exposure algo

--- the-town-game/NPC.gd
+++ the-town-game/NPC.gd
@@ -6,9 +6,19 @@ class_name NPC
 var talked_to: bool = false
 var exposed: bool = false
 @export var exposes: Array[NPC]
-##0: Monday 1: Tuesday 2: Wednesday 3: Thursday 4: Friday 5: Saturday 6: Sunday
-@export var exposure_days: Array[int] = [0,1,2,3,4,5,6]
+##d:hh:mm-d:hh:mm
+@export var exposure: Array[String]
 
 func interact():
 	if not talked_to:
 		talked_to = true
+
+#Put in npc scene!
+func handle_exposure():
+	for i in exposure:
+		if TimeManager.time[0] == i[0]:
+			if TimeManager.time[1] > i.substr(2,3):
+				exposed = true
+			if TimeManager.time[1] == i.substr(2,3):
+				if TimeManager.time[2] >= i.substr(5,6):
+					exposed = true
